476de401ce99f8d202df5b55727172c54b30cf4e
郑松岩(平台开发) <zhengsongyan@BG-U2204-0003.t3caic.com>
2023-12-28 15:13:47 +0800

    merge remote changes

--- source/conf.py
+++ source/conf.py
@@ -14,10 +14,19 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-        'recommonmark',
-        'sphinx_markdown_tables'
-        ]
+#extensions = [
+#        'recommonmark',
+#        'sphinx_markdown_tables'
+#        ]
+
+
+extensions = ["myst_parser"]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
 
 templates_path = ['_templates']
 exclude_patterns = []
