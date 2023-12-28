d36fbeb1308d28c7337e5f408d67d68d0cc84c18
郑松岩(平台开发) <zhengsongyan@BG-U2204-0003.t3caic.com>
2023-12-28 17:38:22 +0800

    new change

--- source/conf.py
+++ source/conf.py
@@ -20,9 +20,9 @@ source_suffix = ['.rst', '.md']
 # Mock modules that are not available on Read the Docs
 MOCK_MODULES = ['recommonmark', 'sphinx_markdown_tables']
 sys.modules.update((mod_name, MagicMock()) for mod_name in MOCK_MODULES)
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'recommonmark', 'sphinx_markdown_tables']
+extensions = ['myst_parser','sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'recommonmark', 'sphinx_markdown_tables']
 
-extensions = ["myst_parser"]
+#extensions = ["myst_parser"]
 
 source_suffix = {
     '.rst': 'restructuredtext',
